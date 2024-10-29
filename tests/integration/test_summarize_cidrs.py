5a43136aad1792ce9f805c26880873bd1589fec0
Valentin Vasilev <valentin.vasilev@sap.com>
2024-10-29 15:57:32 +0200

    :sparkles: Update integration tests
    * Applied pylint recommendations.
    
    Signed-off-by: Valentin Vasilev <valentin.vasilev@sap.com>

--- tests/integration/test_summarize_cidrs.py
+++ tests/integration/test_summarize_cidrs.py
@@ -14,16 +14,15 @@ def test_cidr_summarization():
     expected_output = "192.168.0.0/23\n192.168.2.0/24\n"
 
     # Run the script with subprocess and pass the input data
-    process = subprocess.Popen(
+    with subprocess.Popen(
         [sys.executable, "app/summarize_cidrs.py"],
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,
-    )
-
-    # Feed the input data to the process
-    stdout, stderr = process.communicate(input=input_data)
+    ) as process:
+        # Feed the input data to the process
+        stdout, stderr = process.communicate(input=input_data)
 
     # Assert no error messages and that the output matches the expected output
     assert stderr == "", f"Expected no stderr, but got: {stderr}"
